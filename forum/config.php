2b9ffc49a453f56d38d3700244344b6f571f7c44
Alban Mulaki <alban.mulaki@gmail.com>
2016-03-15 22:51:02 +0100

    Init project

---
+++ forum/config.php
@@ -0,0 +1,17 @@
+<?php
+
+$db_type = 'mysqli_innodb';
+$db_host = 'localhost';
+$db_name = 'albanmul_educationks';
+$db_username = 'albanmul_eduks';
+$db_password = 'Z4eDr&,&c$xK';
+$db_prefix = 'forum_';
+$p_connect = false;
+
+$cookie_name = 'pun_cookie_a90905';
+$cookie_domain = '';
+$cookie_path = '/';
+$cookie_secure = 0;
+$cookie_seed = '0bfaec6782c76ed9';
+
+define('PUN', 1);
